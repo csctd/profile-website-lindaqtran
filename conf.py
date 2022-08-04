d0cb450c286a141a2e57cb12cb63cfecdea86e07
lindaqtran <98556872+lindaqtran@users.noreply.github.com>
2022-08-04 16:00:13 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,8 +1,8 @@
 # -- Project information -----------------------------------------------------
 
-project = 'Site Title'
-copyright = '2022, Your name'
-author = 'Your Name'
+project = 'Linda'
+copyright = '2022, Linda'
+author = 'Linda'
 
 
 
